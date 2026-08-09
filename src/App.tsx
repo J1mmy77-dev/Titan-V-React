23ef7643c17eae3bd484a59139cfbd47e8085eb0
santiagopinzn03-cyber <santiagopinzn03@gmail.com>
2026-08-08 22:17:42 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import { useState } from 'react';
 import Usuarios from './components/Usuarios';
 import Productos from './components/Productos';
-import { Sidebar } from './components/Sidebar';
-import { ProyectosTab } from './components/ProyectosTab';
-import { MaterialesTab } from './components/MaterialesTab';
+import ProyectosTab from './components/ProyectosTab';
+import MaterialesTab from './components/MaterialesTab';
+import Sidebar from './components/Sidebar';
+import Login from './components/Login';
 import './App.css';
 
 function App() {
+ 
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   const [tabActual, setTabActual] = useState<string>('proyectos');
 
   const handleLogout = () => {
+    setIsLoggedIn(false);
     alert('Sesión cerrada correctamente.');
   };
+
+  if (!isLoggedIn) {
+    return <Login onLoginSuccess={() => setIsLoggedIn(true)} />;
+  }
+
 
   return (
     <div style={{ display: 'flex', minHeight: '100vh', backgroundColor: '#f4f6f9' }}>
